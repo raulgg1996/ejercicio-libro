78a7f34bad8c6adfc5c072b013511fb4fbb87693
raul <chavo_cruzazul@hotmail.com>
2017-08-24 10:31:03 -0500

    ya quedo

---
+++ Libro.java
@@ -0,0 +1,12 @@
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    
+    public Libro(String tit,String aut)
+    {
+        titulo=tit;
+        autor=aut;
+    }
+   
+}  
