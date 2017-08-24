124b08befdfef5bcbc175f9ce70e9c5ed4ce75c2
raul <chavo_cruzazul@hotmail.com>
2017-08-24 10:59:13 -0500

    se termino el proyecto hasta el paso 15 pero continuara

--- Libro.java
+++ Libro.java
@@ -2,11 +2,13 @@ public class Libro
 {
     private String titulo;
     private String autor;
+    private int paginas;
     
-    public Libro(String tit,String aut)
+    public Libro(String tit,String aut,int pag)
     {
         titulo=tit;
         autor=aut;
+        paginas=pag;
     }
    
     public String dimeAutor()
@@ -17,5 +19,27 @@ public class Libro
     public String dimeTitulo()
     {
         return titulo;
+    }
+    public int dimePaginas()
+    {
+       
+        return paginas;
+        
+    } 
+    public String dimeDetalles()
+    {
+        String cadResultado="";
+        cadResultado=cadResultado+"Titulo";
+        cadResultado=cadResultado+": ";
+        cadResultado=cadResultado+titulo;
+        cadResultado=cadResultado+"Autor";
+        cadResultado=cadResultado+": ";
+        cadResultado=cadResultado+autor;
+        cadResultado=cadResultado+"Páginas";
+        cadResultado=cadResultado+": ";
+        cadResultado=cadResultado+paginas;
+        
+        return cadResultado;
+        
     }
 }  
